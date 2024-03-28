990c00f68aafc3939b00ec8df5372259931d5cbf
codeStraight <102733329+codeStraight@users.noreply.github.com>
2024-03-29 01:45:09 +0500

    Create switch-statement.php
    THIS FILE CONTAINS SOURCE CODE OF OUR SEVENTH EPISODE OF "PHP with MySQL Crash Course for Beginners: Fast Track to Web Development!" ( SWITCH-STATEMENT ).

---
+++ switch-statement.php
@@ -0,0 +1,34 @@
+<?php
+  // switch statement
+  $day = "Thursday";
+  
+  switch ($day) {
+    case 'Monday':
+      # code...
+      echo "It's the beginning of the week";
+      break;
+    case 'Tuesday':
+      # code...
+      break;
+    case 'Wednesday':
+      # code...
+      break;
+    case 'Thursday':
+      # code...
+      echo "It's a working day";
+      break;
+    case 'Friday':
+      # code...
+      echo "TGIF - Its Friday!";
+      break;
+    
+    default:
+      # code...
+      echo "It's the weekend";
+      break;
+  }
+?>
+
+<?php
+    echo '<p style="margin-top: 10rem; text-align:center">&copy; codeStraight</p>'
+?>
