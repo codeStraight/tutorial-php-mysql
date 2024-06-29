39bdf0d4e8070b771dc4f5988aa6080d7f234d64
codeStraight <102733329+codeStraight@users.noreply.github.com>
2024-06-29 15:27:11 +0500

    Add files via upload
    THIS FILE CONTAINS SOURCE CODE OF OUR TWENTIETH EPISODE OF "PHP WITH MYSQL CRASH COURSE FOR BEGINNERS: FAST TRACK TO WEB DEVELOPMENT!" ( NAMESPACE ).

---
+++ namespace/namespace1.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace order1;
+
+class Order
+{
+    public function echoText()
+    {
+        echo "this text is from namespace1";
+    }
+}
---
+++ namespace/namespace2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace order2;
+
+class Order
+{
+    public function echoText()
+    {
+        echo "this text is from namespace2";
+    }
+}
